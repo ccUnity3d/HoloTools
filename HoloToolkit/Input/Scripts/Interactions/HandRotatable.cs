7d6bc196c0bc45b3a0851f2b048fbd08734f4f80
doris4730 <alexeydorochov@gmail.com>
2017-03-23 14:53:35 +0300

    rework project

---
+++ HoloToolkit/Input/Scripts/Interactions/HandRotatable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule
+{
+    public class HandRotatable : MonoBehaviour, IInputHandler
+    {
+        public bool IsRotationEnabled = true;
+
+        public float RotationSpeed = 0.5f;
+
+        private bool IsPressed;
+
+        private Transform menuParent;
+
+        public void OnInputDown(InputEventData eventData)
+        {
+            IsPressed = true;
+        }
+
+        public void OnInputUp(InputEventData eventData)
+        {
+            IsPressed = false;
+        }
+
+        private void Update()
+        {
+            if (IsRotationEnabled && IsPressed)
+            {
+                transform.Rotate(0, RotationSpeed, 0);
+            }
+        }
+    }
+}
---
+++ HoloToolkit/Input/Tests/Scripts/ButtonObjectScaler.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule.Tests
+{
+    public class ButtonObjectScaler : MonoBehaviour
+    {
+        [SerializeField]
+        private TestButton button = null;
+
+        [SerializeField]
+        private GameObject ObjectToScale = null;
+
+        [SerializeField]
+        private float ScaleIncrement = 1.0f;
+
+        [SerializeField]
+        private buttonAction ButtonAction = buttonAction.Reset;
+
+        private enum buttonAction { Move, Rotate, Bigger, Smaller, Reset };
+
+        private Vector3 InitialScale;
+
+        private HandDraggable handDraggable;
+
+        private HandRotatable handRotatable;
+
+        private void Start()
+        {
+            if (ObjectToScale)
+            {
+                InitialScale = ObjectToScale.transform.localScale;
+                handDraggable = ObjectToScale.GetComponent<HandDraggable>();
+                handRotatable = ObjectToScale.GetComponent<HandRotatable>();
+            }
+        }
+
+        private void OnEnable()
+        {
+            button.Activated += OnButtonPressed;
+        }
+
+        private void OnDisable()
+        {
+            button.Activated -= OnButtonPressed;
+        }
+
+        private void OnButtonPressed(TestButton source)
+        {
+            if (ObjectToScale)
+            {
+                switch (ButtonAction)
+                {
+                    case buttonAction.Move:
+                        if (handRotatable)
+                        {
+                            handRotatable.IsRotationEnabled = false;
+                        }
+                        if (handDraggable)
+                        {
+                            handDraggable.IsDraggingEnabled = !handDraggable.IsDraggingEnabled;
+                        }
+                        break;
+
+                    case buttonAction.Rotate:
+                        if (handDraggable)
+                        {
+                            handDraggable.IsDraggingEnabled = false;
+                        }
+                        if (handRotatable)
+                        {
+                            handRotatable.IsRotationEnabled = !handRotatable.IsRotationEnabled;
+                        }
+                        break;
+
+                    case buttonAction.Bigger:
+                        ObjectToScale.transform.localScale = new Vector3((ObjectToScale.transform.localScale.x + ScaleIncrement), (ObjectToScale.transform.localScale.y + ScaleIncrement), (ObjectToScale.transform.localScale.z + ScaleIncrement));
+                        break;
+
+                    case buttonAction.Smaller:
+                        if ((ObjectToScale.transform.localScale.x - ScaleIncrement) < 0.0f)
+                        {
+                            ObjectToScale.transform.localScale = new Vector3(0, 0, 0);
+                        }
+                        else
+                        {
+                            ObjectToScale.transform.localScale = new Vector3((ObjectToScale.transform.localScale.x - ScaleIncrement), (ObjectToScale.transform.localScale.y - ScaleIncrement), (ObjectToScale.transform.localScale.z - ScaleIncrement));
+                        }
+                        break;
+
+                    case buttonAction.Reset:
+                        Debug.Log(InitialScale);
+                        ObjectToScale.transform.localScale = InitialScale;
+                        break;
+                }
+            }
+
+            button.Selected = false;
+        }
+    }
+}
